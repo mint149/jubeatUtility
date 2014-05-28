2f5c3c02fb6515ce7e7662abb5ae10bdcd352d0e
Mint <skylove.exe@gmail.com>
2014-05-28 18:00:50 +0900

    数値入力実装。 許容グレ数計算の時、テキストフィールドの入力を見るようになった。

--- jubeatUtility/FirstViewController.m
+++ jubeatUtility/FirstViewController.m
@@ -12,6 +12,13 @@
 
 @end
 
+UILabel *noteLabel;
+UILabel *scoreLabel;
+UILabel *greatLabel;
+UITextField *noteTextField;
+UITextField *scoreTextField;
+UITextField *greatTextField;
+
 @implementation FirstViewController
 
 - (void)viewDidLoad
@@ -22,32 +29,32 @@
 	//TODO:キーボードをnumberpadに変更。
 	//TODO:レイアウトの方法を調べる
 	//	TODO:計算メソッドの中身を実装。
-	UILabel *noteLabel = [[UILabel alloc] init];
+	noteLabel = [[UILabel alloc] init];
 	noteLabel.frame = CGRectMake(10, 10, 100, 50);
 	noteLabel.text = @"ノーツ数";
 	[self.view addSubview:noteLabel];
 	
-	UILabel *scoreLabel = [[UILabel alloc] init];
+	scoreLabel = [[UILabel alloc] init];
 	scoreLabel.frame = CGRectMake(10, 60, 100, 100);
 	scoreLabel.text = @"目標スコア";
 	[self.view addSubview:scoreLabel];
 	
-	UILabel *greatLabel = [[UILabel alloc] init];
+	greatLabel = [[UILabel alloc] init];
 	greatLabel.frame = CGRectMake(10, 110, 100, 150);
 	greatLabel.text = @"許容グレ数";
 	[self.view addSubview:greatLabel];
 	
-	UITextField *noteTextField = [[UITextField alloc]init];
+	noteTextField = [[UITextField alloc]init];
 	noteTextField.frame = CGRectMake(110, 10, 200, 50);
 	noteTextField.placeholder = @"ノーツ数を入力";
 	[self.view addSubview:noteTextField];
 	
-	UITextField *scoreTextField = [[UITextField alloc]init];
+	scoreTextField = [[UITextField alloc]init];
 	scoreTextField.frame = CGRectMake(110, 60, 200, 100);
 	scoreTextField.placeholder = @"目標スコアを入力";
 	[self.view addSubview:scoreTextField];
 	
-	UITextField *greatTextField = [[UITextField alloc]init];
+	greatTextField = [[UITextField alloc]init];
 	greatTextField.frame = CGRectMake(110, 110, 200, 150);
 	greatTextField.placeholder = @"許容グレ数";
 	[self.view addSubview:greatTextField];
@@ -69,11 +76,11 @@
 }
 
 - (void)calc:(UIButton*)button{
-	int notes = 700;
-	int targetScore = 950000;
+	int notes = [noteTextField.text intValue];
+	int targetScore = [scoreTextField.text intValue];
 	int allowGreat = 0;
 	for (allowGreat = 0; allowGreat < notes; allowGreat++) {
-		double a = 1000000 * (notes - allowGreat) / notes;
+		double a = 1000000 * (notes - 0.3*allowGreat) / notes;
 		double b = 0.9 * (int)a;
 		int c = (int)b + 100000;
 		NSLog(@"%d",c);
@@ -83,6 +90,7 @@
 		}
 	}
 	NSLog(@"%d",allowGreat);
+	greatTextField.text = [NSString stringWithFormat:@"%d",allowGreat];
 }
 
 @end
